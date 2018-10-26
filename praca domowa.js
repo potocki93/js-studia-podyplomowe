641c31aae07afe6271a3f532c34e37e5260d332d
potocki93 <44320302+potocki93@users.noreply.github.com>
2018-10-26 16:59:01 +0200

    Add files via upload

---
+++ praca domowa.js
@@ -0,0 +1,92 @@
+// zadanie 3
+
+// let SumaNieparzystych = 0
+// let liczby = [1,6,23,8,4,98,3,7,3,98,4,98]
+
+// for (let i = 0; i < liczby.length; i++) {
+//     if (liczby[i] % 2 !==0) {
+//         SumaNieparzystych = SumaNieparzystych + liczby[i]
+//     }
+// }
+// console.log(SumaNieparzystych)
+
+// zadanie 4
+
+// let liczby = [1,6,23,8,4,98,3,7,3,98,4,98]
+// let max = liczby[0]
+// let min = liczby[0]
+
+// for (let i = 0; i < liczby.length; i++) {
+//     if(liczby[i]<min)
+//     {
+//         min = liczby[i]
+//     }
+
+// }
+//   console.log(min);
+
+// zadanie 5
+
+// let wyrazy = ['Karol','Adam','Rogowski','Politechnika','Super','Weekend']
+// let dł = 0;
+// let najdł;
+
+// for (let i = 0; i < wyrazy.length; i++) {
+//     if (wyrazy[i].length > dł) {
+//         dł = wyrazy[i].length;
+//         najdł = wyrazy[i];
+//     }
+// }
+// console.log(najdł);
+// console.log(dł);
+// console.log(wyrazy.length)
+
+// zadanie 6
+
+// let liczby = [1,6,23,8,4,98,3,7,3,98,4,98]
+// let val = 0
+// let MaxValIndex;
+
+// for (let i = 0; i < liczby.length; i++) {
+//     if (liczby[i] > val) {
+        
+//         val = liczby[i];
+//         MaxValIndex = liczby.indexOf(val);
+//     }
+// }
+
+// console.log(MaxValIndex);
+
+//zadanie 7
+
+// let liczby = [1,6,23,8,4,98,3,7,3,98,4,98]
+// let SumEven = 0;
+
+// let EvenNum = liczby.filter(function(el){
+//     return el % 2 ===0;
+// });
+// console.log(EvenNum);
+
+// for (let i = 0; i < EvenNum.length; i++) {
+//         SumEven = SumEven + EvenNum[i]
+//     }
+// console.log(SumEven / EvenNum.length);
+
+//zadanie 8
+
+// let liczby = [1,6,23,8,4,98,3,7,3,98,4,98]
+// let Suma = 0;
+// let ilość = 0;
+
+// for (let i = 1; i<liczby.length; i++){
+//     if((liczby.indexOf(liczby[i])) % 2 === 0){
+//         Suma = Suma + liczby[i];
+//         ilość = ilość + 1
+//     }   
+// }
+// console.log(Suma/ilość)
+
+//zadanie 9
+
+
+
